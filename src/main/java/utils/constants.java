2e58a9ccd83bfab2d5469da6c5c615f712fb3278
Vijay Vignesh <rush2vijay.g@gmail.com>
2021-04-07 13:28:52 +0100

    Added basic structure and Webdriver codes

---
+++ src/main/java/utils/constants.java
@@ -0,0 +1,4 @@
+package main.java.utils;
+
+public interface constants {
+}
---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,52 @@
+package test.java;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+import java.io.File;
+
+public class BaseTest {
+
+    public static WebDriver driver;
+
+    @BeforeTest
+    public void beforeTest(){
+
+
+    }
+
+    @BeforeMethod
+    @Parameters(value={"browserName"})
+    public void beforeMethod(String browserName){
+        setUpDriver(browserName);
+        driver.manage().window().maximize();
+        driver.get("www.amazon.co.uk");
+    }
+
+
+    @AfterMethod
+    public void afterMethod(){
+
+
+    }
+
+
+
+    @AfterTest
+    public void afterTest(){
+
+
+    }
+
+
+    public void setUpDriver(String browser){
+        if (browser.equalsIgnoreCase("chrome")) {
+            System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + File.separator + "drivers" + File.separator + "chromedriver");
+            driver = new ChromeDriver();
+        } else if(browser.equalsIgnoreCase("firefox")){
+            System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + File.separator + "drivers" + File.separator + "geckodriver");
+            driver = new ChromeDriver();
+        }
+    }
+}
